7de417749e7d6bdf1eacc1eda13f5185bc523794
Jay Tuckey <jay.tuckey@gmail.com>
2013-03-26 21:51:26 +0930

    Added a bunch of neccessary stuff. Basically all changes start from now.

--- src/com/flightgearplanes/supersimplenotes/ActivityItemViewer.java
+++ src/com/flightgearplanes/supersimplenotes/ActivityItemViewer.java
@@ -6,7 +6,6 @@ import android.annotation.SuppressLint;
 import android.app.ActionBar;
 import android.content.ContentValues;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
